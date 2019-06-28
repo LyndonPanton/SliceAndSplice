335496a760a4b46d47d45c1c548c16b82f48b1f8
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-28 16:52:22 +0100

    Added logic to display first array, second array, index, result

--- js/app.js
+++ js/app.js
@@ -10,12 +10,37 @@ window.onload = function(event) {
 	// Edit me (index to insert at)
 	let n = 4;
 
+	function display(results) {
+		let d1 = document.getElementById("display-one-value");
+		let d2 = document.getElementById("display-two-value");
+		let i = document.getElementById("display-index-value");
+		let r = document.getElementById("display-three-value");
+
+		d1.textContent = "???";
+		d2.textContent = "???";
+		i.textContent = "???";
+
+		if (!Array.isArray(results[0]) || !Array.isArray(results[1])) {
+			r.textContent = "Both elements must be arrays";
+		} else if (!results[0].length || !results[1].length) {
+			r.textContent = "Arrays must have values entered";
+		} else if (typeof(results[2]) !== "number") {
+			r.textContent = "Insertion values must be a number";
+		} else {
+			d1.textContent = "[" + results[0].join(", ") + "]";
+			d2.textContent = "[" + results[1].join(", ") + "]";
+			i.textContent = results[2];
+			// r.textContent = "...";
+			r.textContent = "[" + results[3].join(", ") + "]";
+		}
+	}
+
 	function splicer(first, second, insertion) {
 		let result = second.slice();
 		result.splice(insertion, 0, ...arr1);
 
-		console.log(result);
-		return result;
+		console.log([first, second, insertion, result]);
+		display([first, second, insertion, result]);
 	}
 
 	splicer(arr1, arr2, n);
